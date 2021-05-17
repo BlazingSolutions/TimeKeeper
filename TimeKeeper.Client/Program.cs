d63c78140e6e81db05063182aa7e387402da7233
Jon Hilton <hilton.jon@gmail.com>
2021-05-17 21:23:30 +0100

    Fixed projects to use the IIS Express ports as that works better for folks launching the project via Visual Studio (need to think about how best to manage these ports in general)

--- TimeKeeper.Client/Program.cs
+++ TimeKeeper.Client/Program.cs
@@ -16,7 +16,7 @@ namespace TimeKeeper.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddRefitClient<ITimeEntryApi>()
-                .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:5001/"); });
+                .ConfigureHttpClient(c => { c.BaseAddress = new Uri("https://localhost:44331/"); });
 
             builder.Services.AddMudServices();
 
