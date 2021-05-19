472304b2919564c79ca171e075d06ce1f236e484
francisco.villena <francisco.dublin@gmail.com>
2021-05-20 00:08:56 +0100

    Add handler for delete command

--- TimeKeeper.Shared/Api/Features/TimeEntry/Delete.cs
+++ TimeKeeper.Shared/Api/Features/TimeEntry/Delete.cs
@@ -4,7 +4,7 @@ namespace TimeKeeper.Shared.Api.Features.TimeEntry
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<int>
         {
             public int Id { get; set; }
         }
